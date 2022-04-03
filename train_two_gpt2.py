a286d45b3fa4a318008042f7d2a69f7d84fdd979
Justus Mattern <jmattern@eu-login-38.euler.ethz.ch>
2022-04-03 17:47:19 +0200

    simplified train script

--- train_two_gpt2.py
+++ train_two_gpt2.py
@@ -1,22 +1,3 @@
-from transformers import GPT2Tokenizer
-import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
-
-
-class TorchTokenizer(GPT2Tokenizer):
-
-    def __call__(self, sentence):
-        return [self.encode(s, max_length=1024, truncation=True, return_tensors='pt') for s in sentence]
-
-tokenizer = TorchTokenizer.from_pretrained('gpt2-large')
-special_tokens_dict = {'bos_token': '<BOS>', 'eos_token': '<EOS>', 'pad_token': '<PAD>'}
-num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
-
-#print(tokenizer(['<|endoftext|> This is my cute dog']))
-#print(tokenizer.encode('<|endoftext|> This is my cute dog', max_length=1024, truncation=True, return_tensors='pt'))
-
-
 from transformers import (
 AutoModelWithLMHead,
 AutoConfig,
@@ -26,19 +7,10 @@ TextDataset,
 DataCollatorForLanguageModeling,
 TrainingArguments)
 
-import os
+def train_model(text_path, epochs, model_name="gpt2-xl", batch_size=1, cache_dir = "cache"):
 
-def modelTrainer(text_path, epochs, model="gpt2-large", batch_size=1, cache_dir = "cache"):
-
-    model = AutoModelWithLMHead.from_pretrained(model)
-    #for name, param in model.named_parameters():
-         #nums = list(range(35,48))
-         #if not any(str(num) in name for num in nums):
-         #    param.requires_grad = False
-         #    print(name)
-
-    tokenizer = AutoTokenizer.from_pretrained('gpt2-large')
-    tokenizer = TorchTokenizer.from_pretrained('gpt2-large')
+    model = AutoModelWithLMHead.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     special_tokens_dict = {'bos_token': '<BOS>', 'eos_token': '<EOS>', 'pad_token': '<PAD>'}
     num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
     model.resize_token_embeddings(len(tokenizer))
@@ -51,7 +23,7 @@ def modelTrainer(text_path, epochs, model="gpt2-large", batch_size=1, cache_dir 
     )
     
     training_args =TrainingArguments(
-    output_dir="model2 {}".format(os.path.basename(text_path)),
+    output_dir="trained_model",
     num_train_epochs=epochs,
     per_device_train_batch_size=batch_size,
     warmup_steps=500,
@@ -71,79 +43,3 @@ def modelTrainer(text_path, epochs, model="gpt2-large", batch_size=1, cache_dir 
     trainer.save_model()
 
 
-with open('data/imdb_train.txt', 'r') as f:
-    with open('data/train_model.txt', 'w') as f2:
-        count = 0
-        for line in f:
-            count += 1
-            if int(line.split(' ')[0]) == 1:
-                f2.write('<BOS> Write a positive review about a great movie: '+' '.join(line.split(' ')[1:]).replace('\n', '')+' <EOS>\n\n')
-            else:
-                f2.write('<BOS> Write a negative review about a bad movie: '+' '.join(line.split(' ')[1:]).replace('\n', '')+' <EOS>\n\n')
-            if count > 50:
-                break
-
-
-modelTrainer('data/train_model.txt', 3, model='gpt2-large')
-
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
-
-test_pos = []
-test_neg = []
-
-dataset = []
-with open('data/test.txt', 'r') as f:
-    for line in f:
-        dataset.append((' '.join(line.split(' ')[1:]).replace('\n', ''), int(line.split(' ')[0])))
-
-"""
-import math
-import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-import random
-# Load pre-trained model (weights)
-model_pos = GPT2LMHeadModel.from_pretrained('./model yelp_positive_train.txt')
-
-test_data = test_pos+test_neg
-random.shuffle(test_data)
-
-model_pos.eval()
-model_neg.eval()
-
-# Load pre-trained model tokenizer (vocabulary)
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
-def score(sentence, model):
-    tensor_input = tokenizer.encode(sentence, max_length=1024, truncation=True, return_tensors='pt')
-    loss=model(tensor_input, labels=tensor_input)[0]
-    return math.exp(loss)
-
-print('len test', len(test_data))
-
-pos_scores = []
-neg_scores = []
-rights = 0
-preds = []
-for i, (rev, lab) in enumerate(test_data[:1000]):
-    if i % 100 == 0:
-      print(i)
-    print(rev)
-    print(lab)
-    pos = score(rev, model_pos)
-    neg = score(rev, model_neg)
-    pos_scores.append(pos)
-    neg_scores.append(neg)
-    if pos > neg:
-      preds.append(0)
-      if lab == 0:
-        rights += 1
-    else:
-      preds.append(1)
-      if lab == 1:
-        rights +=1
-    
-
-print('acc', rights/len(test_data[:1000]))
-
-
-"""
