135abd3daf8a2f1a53b415b1db153a3d72491fdc
justusmattern <justus_mattern@gmx.de>
2022-04-03 23:49:38 +0200

    fix data path

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import os
 
 def write_texts_to_file(texts, dir, filename):
     os.makedirs(dir, exist_ok=True)
-    with open(f'{dir}/{filename}_training_data.txt', 'w') as f:
+    with open(f'{dir}/{filename}', 'w') as f:
         for text in texts:
             f.write(text+'\n') 
     
